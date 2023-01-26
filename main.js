2eb2ef27a80eef084426c45742181b10e62db2a5
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-01-26 16:04:20 +0100

    dadi

--- main.js
+++ main.js
@@ -3,22 +3,39 @@
 //se email é uguale email nella lista stampo alert
 //se email  non é uguale email nella lista stampo alert diverso
 
-const email = [
-    "ansia@ghghg",
-    "strepidazione@gjgj",
-    "addio"
-];
-const text = prompt("la tua email ?")
-console.log(text);
+// const email = [
+//     "ansia@ghghg",
+//     "strepidazione@gjgj",
+//     "addio"
+// ];
+// const text = prompt("la tua email ?")
+// console.log(text);
 
-for (let i = 1; i < email.length; i++) {
-    console.log(email[i]);
-    if (text[i] == email[i]) {
-        alert("bravo,la tua email é giusta");
-    }
-    else if (text[i] != email[i]) {
-        alert("Hai dimenticato la tua email?riprova");
-    }
+// for (let i = 1; i < email.length; i++) {
+//     console.log(email[i]);
+//     if (text == email[i]) {
+//         alert("bravo,la tua email é giusta");
+//     }
+//     if (text != email[i]) {
+//         alert("Hai dimenticato la tua email?riprova");
+//     }
+// }
+
+// dadi
+// diamo numeri random a me e al computer
+const mycomputer = Math.floor(Math.random() * 6) + 1;
+console.log(mycomputer);
+const person = Math.floor(Math.random() * 6) + 1;
+console.log(person);
+// 
+if (mycomputer > person) {
+    alert("ha vinto il computer");
+}
+if (person < mycomputer) {
+    alert("hai perso");
+}
+else if (mycomputer == person) {
+    alert("try again")
 
 
 }
