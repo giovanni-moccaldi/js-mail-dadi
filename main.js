69efa4ac8ce233098220342dd5164c7cde036feb
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-01-28 13:35:41 +0100

    no works

--- main.js
+++ main.js
@@ -4,19 +4,23 @@
 //se email  non é uguale email nella lista stampo alert diverso
 
 const email = [
+    "trovato@gmgm",
     "ansia@ghghg",
     "strepidazione@gjgj",
     "addio"
 ];
-const text = prompt("la tua email ?")
-console.log(text);
+let text = prompt("la tua email ?")
+// console.log(typeof text);
+let trovato = false;
 
-for (let i = 1; i < email.length; i++) {
-    console.log(email[i]);
+for (let i = 0; i < email.length && trovato == false; i++) {
+
     if (text == email[i]) {
         alert("bravo,la tua email é giusta");
+        trovato = true;
+
     }
-    if (text != email[i]) {
+    if (trovato == false) {
         alert("Hai dimenticato la tua email?riprova");
     }
 }
@@ -25,24 +29,24 @@ for (let i = 1; i < email.length; i++) {
 // diamo numeri random a me e al computer
 
 
-const person = Math.floor(Math.random() * 6) + 1;
-const mycomputer = Math.floor(Math.random() * 6) + 1;
-console.log(mycomputer);
-console.log(person)
+// const person = Math.floor(Math.random() * 6) + 1;
+// const mycomputer = Math.floor(Math.random() * 6) + 1;
+// console.log(mycomputer);
+// console.log(person)
 
-// fare la condizione
-if (mycomputer > person) {
-    alert("ha vinto il computer");
-
-
-}
-if (person < mycomputer) {
-    alert("hai vinto");
+// // fare la condizione
+// if (mycomputer > person) {
+//     alert("ha vinto il computer");
 
 
-}
-else if (mycomputer == person) {
-    alert("try again")
+// }
+// if (person < mycomputer) {
+//     alert("hai vinto");
 
 
-}
+// }
+// else if (mycomputer == person) {
+//     alert("try again")
+
+
+// }
